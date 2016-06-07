06480eaa882a52b924e0f2a62c3d2c5fd0424dcb
Martin Zugnoni <martin.zugnoni@gmail.com>
2016-06-06 22:40:03 -0300

    Implement pending views

--- twitter_clone/main.py
+++ twitter_clone/main.py
@@ -55,16 +55,17 @@ def login():
     next = request.args.get('next', '/')
 
     if 'username' in session:
-        flash('You are actually logged in, there is no need to login again', 'warning')
+        # already logged in
         return redirect(next)
 
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         cursor = g.db.execute(
-            'SELECT password FROM user WHERE username=:username;', {'username': username})
+            'SELECT id, password FROM user WHERE username=:username;', {'username': username})
         user = cursor.fetchone()
-        if user and user[0] == md5(password).hexdigest():
+        if user and user[1] == md5(password).hexdigest():
+            session['user_id'] = user[0]
             session['username'] = username
             flash('You were correctly logged in', 'success')
             return redirect(next)
@@ -79,6 +80,7 @@ def login():
 @login_required
 def logout():
     next = request.args.get('next', '/')
+    session.pop('user_id', None)
     session.pop('username', None)
     flash('You were correctly logged out', 'info')
     return redirect(next)
@@ -109,3 +111,56 @@ def profile():
     username, first_name, last_name, birth_date = cursor.fetchone()
     return render_template('profile.html', username=username, first_name=first_name,
                            last_name=last_name, birth_date=birth_date)
+
+
+@app.route('/<username>', methods=['GET', 'POST'])
+def feed(username):
+    if request.method == 'POST':
+        if 'username' not in session:
+            # user not logged in
+            return render_template('403.html'), 403
+
+        tweet = request.form.get('tweet')
+        if not tweet:
+            flash('Please write something in your tweet', 'danger')
+        else:
+            query = 'INSERT INTO tweet ("user_id", "content") VALUES (:user_id, :content);'
+            params = {'user_id': session['user_id'], 'content': tweet}
+            try:
+                g.db.execute(query, params)
+                g.db.commit()
+            except sqlite3.IntegrityError:
+                flash('Something went wrong while saving your tweet', 'danger')
+            else:
+                flash('Thanks for your tweet!', 'success')
+
+    # check if given username exists
+    cursor = g.db.execute(
+        'SELECT id FROM user WHERE username=:username;', {'username': username})
+    user = cursor.fetchone()
+    if not user:
+        return render_template('404.html'), 404
+
+    # fetch all tweets from given username
+    cursor = g.db.execute(
+        'SELECT u.username, u.first_name, u.last_name, t.id, t.created, t.content FROM user AS u JOIN tweet t ON (u.id=t.user_id) WHERE u.username=:username ORDER BY datetime(created) DESC;',
+        {'username': username})
+    tweets = [dict(username=row[0], id=row[3], created=row[4], content=row[5]) for row in cursor.fetchall()]
+    return render_template('feed.html', feed_username=username, tweets=tweets)
+
+
+@app.route('/tweets/<int:tweet_id>/delete', methods=['POST'])
+@login_required
+def tweet(tweet_id):
+    next = request.args.get('next', '/')
+    cursor = g.db.execute(
+        'SELECT * FROM tweet WHERE id=:tweet_id AND user_id=:user_id;',
+        {'tweet_id': tweet_id, 'user_id': session['user_id']})
+    if not cursor.fetchone():
+        return render_template('404.html'), 404
+    g.db.execute(
+        'DELETE FROM tweet WHERE id=:tweet_id AND user_id=:user_id',
+        {'tweet_id': tweet_id, 'user_id': session['user_id']})
+    g.db.commit()
+    flash('Your tweet was successfully deleted!', 'success')
+    return redirect(next)
